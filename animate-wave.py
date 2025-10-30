49b7c7a588b819bf475d4202375ccbb4c91e0dfc
henrik <henriktomt@gmail.com>
2025-10-30 13:03:35 +0100

    Legger til større fft-vindu funksjon, samt x_lim velger

--- animate-wave.py
+++ animate-wave.py
@@ -5,7 +5,7 @@ matplotlib.use("QtAgg")  # må stå FØR pyplot importeres
 import matplotlib.pyplot as plt
 from numpy.fft import fftfreq
 from scipy.io import wavfile
-from scipy.fft import fft, ifft
+from scipy.fft import fft, ifft, rfft, rfftfreq
 from matplotlib.animation import FuncAnimation
 import subprocess as sp
 import progressbar
@@ -13,7 +13,9 @@ import os
 import argparse
 
 
-def animate_func(input_file: str, output_file: str, fps: int):
+def animate_func(
+    input_file: str, output_file: str, fps: int, fft_time: float, x_lim: int
+):
 
     # Read input file
     sample_rate, wav = wavfile.read(input_file)
@@ -27,56 +29,84 @@ def animate_func(input_file: str, output_file: str, fps: int):
     # Hvis to kanaler, legg dem sammen og normaliser
     if len(wav.shape) == 2:
         wav = np.mean(wav, axis=1)  # legg sammen
-    spf = int(
-        round(sample_rate / fps)
+    spf = max(
+        1, int(round(sample_rate / fps))
     )  # samples per frame, hvor stor mange samples fft-en skal ta for seg
+
+    hop = int(fft_time * sample_rate)
     T = 1.0 / sample_rate  # Tidsmellomrom mellom samples
-    N = int(round(spf))
+    N = max(2, int(round(hop)))
+
+    if x_lim == 0:
+        x_lim = int(sample_rate // 2)
 
     total_frames = max(
-        0, (len(wav) - spf) // spf + 1
+        0, (len(wav) - N) // spf + 1
     )  # Hvor mange frames / bilder det er totalt
 
     print(
-        f"INPUT FILE:{input_file}\nOUTPUT FILE:{output_file}\nSAMPLE RATE: {sample_rate}\nNUM SAMPLES: {num_samples}\nTOTAL FRAMES: {total_frames}\nSPF / N: {spf}\nT: {T}\n"
+        f"INPUT FILE:{input_file}\nOUTPUT FILE:{output_file}\nSAMPLE RATE: {sample_rate}\nNUM SAMPLES: {num_samples}\nTOTAL FRAMES: {total_frames}\nSPF / N: {spf} / {N}\nT: {T}\n"
     )
 
     # Progressbar for å se status av rendering
-    bar = progressbar.ProgressBar(maxval=total_frames)
-    print("PROGRESS FOURIER TRANSFORM:")
-    bar.start()
+    widgets = [
+        "Frame: ",
+        progressbar.Counter(),
+        "/",
+        str(total_frames),
+        " ",
+        progressbar.Percentage(),
+        " ",
+        progressbar.Bar(marker="█"),
+        " ",
+        progressbar.ETA(),
+        " ",
+        progressbar.Timer(),
+        " ",
+        progressbar.FileTransferSpeed(unit="frames"),
+    ]
 
     # Lage liste for verdier langs x-aksen
-    xf = fftfreq(N, T)[: N // 2]
+    xf = rfftfreq(N, T)[: N // 2]
 
     # Regne ut ny fft for hvert bilde
     def fft_wav(n):
-        n0 = int(n) * spf  # Start sample -n
-        n1 = n0 + spf  # slutt sample n
+        n0 = max(0, int(n) * spf - N // 2)  # Start sample -n
+        n1 = min(len(wav), n0 + N)  # slutt sample n
 
         fftwav = wav[n0:n1]  # Kutte wav-filen fra n0 til n1
 
         if len(fftwav) < N:
-            fftwav = np.pad(seq, (0, N - len(fftwav)))
-        yf = fft(fftwav)  # fft av denkuttet den
+            fftwav = np.pad(fftwav, (0, N - len(fftwav)))
+        yf = rfft(fftwav)  # fft av denkuttet den
         yf = 2.0 / N * np.abs(yf[0 : N // 2])  # Fjerne negative verdier
         return yf
 
-    # Lag figur til plot
-    fig = plt.figure()
+    print("PROGRESS MAX AMPLITUDE")
+    maxAmpBar = progressbar.ProgressBar(maxval=total_frames, widgets=widgets)
+    maxAmpBar.start()
 
     # precompute maks y
     ymax = 1e-9
     for i in range(total_frames):
         y = fft_wav(i)  # samme som i animate
         ymax = max(ymax, np.max(y))
+        maxAmpBar.update(i)
 
+    maxAmpBar.finish()
+
+    bar = progressbar.ProgressBar(maxval=total_frames, widgets=widgets)
+    print("PROGRESS FOURIER TRANSFORM:")
+    bar.start()
+
+    # Lag figur til plot
+    fig = plt.figure()
     # Sett på akse - begrensninger
-    axis = plt.axes(xlim=(0, 4000), ylim=(0, 1.1 * ymax))
+    axis = plt.axes(xlim=(0, x_lim), ylim=(0, 1.1 * ymax))
     axis.set_xlabel("Frekvens [Hz]")
     axis.set_ylabel("Amplitude")
 
-    (line,) = axis.plot([], [], lw=3)
+    (line,) = axis.plot([], [], lw=1)
 
     # initialiser animasjon linje
     def init():
@@ -157,18 +187,40 @@ def get_arguments():
         default=30,
     )
 
+    parser.add_argument(
+        "-t",
+        "--time",
+        help="Write the time frame to fourier transform for each fft, if time = 0, use time = 1 / fps",
+        type=float,
+        default=0.0,
+    )
+
+    parser.add_argument(
+        "-x",
+        "--x_lim",
+        help="The maximum frequency to plot on the x - axis, if 0, use xlim = sample_rate / 2",
+        type=int,
+        default=0,
+    )
+
     args = parser.parse_args()
 
     input_file = args.input
     output_file = args.output
     fps = args.fps
-    return input_file, output_file, fps
+    time = args.time
+    x_lim = args.x_lim
+
+    if time == 0:
+        time = 1 / fps
+
+    return input_file, output_file, fps, time, x_lim
 
 
 def main():
 
-    in_file, out_file, fps = get_arguments()
-    animate_func(in_file, out_file, fps)
+    in_file, out_file, fps, time, x_lim = get_arguments()
+    animate_func(in_file, out_file, fps, time, x_lim)
 
 
 if __name__ == "__main__":
